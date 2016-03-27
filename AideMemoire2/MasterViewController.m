e5fe8027933a639a0ac3e82465c7f5533f9d740d
David Ruiz <cavapaslatet@gmail.com>
2016-03-27 16:42:55 -0700

    ok

--- AideMemoire2/MasterViewController.m
+++ AideMemoire2/MasterViewController.m
@@ -24,6 +24,7 @@
     self.navigationItem.rightBarButtonItem = addButton;
     self.detailViewController = (DetailViewController *)[[self.splitViewController.viewControllers lastObject] topViewController];
     //some comment
+
 }
 
 - (void)viewWillAppear:(BOOL)animated {
