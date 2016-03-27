23544563bcefe672aab2a68b80f01b77017e5673
David Ruiz <cavapaslatet@gmail.com>
2016-03-27 16:29:39 -0700

    comment

--- AideMemoire2/MasterViewController.m
+++ AideMemoire2/MasterViewController.m
@@ -23,6 +23,7 @@
     UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
     self.navigationItem.rightBarButtonItem = addButton;
     self.detailViewController = (DetailViewController *)[[self.splitViewController.viewControllers lastObject] topViewController];
+    //some comment
 }
 
 - (void)viewWillAppear:(BOOL)animated {
